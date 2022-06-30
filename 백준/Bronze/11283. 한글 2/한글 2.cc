01d54c9fa162b20ea9015714af5b10fb76ccc8b8
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2022-06-30 12:05:02 +0900

    [Bronze IV] Title: 한글 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    string s;
+    int N;
+    cin>>s;
+	N=((s[0]&255)-234)*4096+((s[1]&255)-176)*64+(s[2]&255)-127;
+	cout<<N;
+    return (0);
+}
