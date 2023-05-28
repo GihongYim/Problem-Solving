658030d0f4999af92744c009c4afdb0ebfbf58a0
ghyim <56685220+GihongYim@users.noreply.github.com>
2023-05-28 10:50:41 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 4.09 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    answer.push_back(n);
+    while (n != 1)
+    {
+        if (n % 2 == 1)
+            n = n * 3 + 1;
+        else
+            n = n / 2;
+        answer.push_back(n);
+    }
+    return answer;
+}
