b58fd0d2a416920891e7df1c0ae5bde0749838b4
ghyim <56685220+GihongYim@users.noreply.github.com>
2023-06-09 08:16:18 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer(52);
+    for (unsigned int i = 0; i < my_string.length(); i++){
+        if (my_string[i] >= 'A' && my_string[i] <= 'Z')
+            answer[my_string[i] - 'A']++;
+        if (my_string[i] >= 'a' && my_string[i] <= 'z')
+            answer[my_string[i] - 'a' + 26]++;
+    }
+    return answer;
+}
