6111586933db381a9a2332bfe43981a366fd7571
ghyim <56685220+GihongYim@users.noreply.github.com>
2023-08-20 12:19:00 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.46 ms, Memory: 4.27 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int limit, int power) {
+    int answer = 1;
+    int divisors= 0;
+    for (int n = 2; n <= number; n++) {
+        divisors= 2;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0)
+                divisors++;
+        }
+        if (divisors > limit)
+            answer += power;
+        else
+            answer += divisors;
+    }
+    return answer;
+}
