a776c251b46a507fdca09c559976daafd2b74f67
ghyim <56685220+GihongYim@users.noreply.github.com>
2023-12-11 10:53:56 +0900

    [Silver IV] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+
+	cin >> N;
+
+	if (N % 2 == 0) {
+		cout << "SK\n";
+	} else {
+		cout << "CY\n";
+	}
+
+	return 0;
+}
