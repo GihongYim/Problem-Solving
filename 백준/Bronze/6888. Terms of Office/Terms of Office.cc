c12c97875812f68b08a1e8f44a36d2772c836722
ghyim <56685220+GihongYim@users.noreply.github.com>
2024-02-22 11:46:18 +0900

    [Bronze IV] Title: Terms of Office, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6888. Terms of Office/Terms of Office.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int x, y;
+
+    cin >> x >> y;
+    for (int i = x; i <= y; i += 60) {
+        cout << "All positions change in year " << i << '\n';
+    }
+
+    return 0;
+}
