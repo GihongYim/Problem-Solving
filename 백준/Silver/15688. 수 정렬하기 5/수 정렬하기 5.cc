e02382a83d1454c48df670d2f694bbf085747bcd
ghyim <56685220+GihongYim@users.noreply.github.com>
2024-03-29 17:01:25 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4644 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int N;
+    
+    cin >> N;
+    vector<int> a(N);
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    for (int i = 0; i < N; i++) {
+        cout << a[i] << '\n';
+    }
+
+    return 0;
+}
