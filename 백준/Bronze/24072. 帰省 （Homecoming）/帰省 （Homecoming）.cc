ab2675e4a57c27104f661647e422d4fdd8a40d9b
ghyim <56685220+GihongYim@users.noreply.github.com>
2024-04-11 11:35:48 +0900

    [Bronze IV] Title: 帰省 (Homecoming), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24072. 帰省 （Homecoming）/帰省 （Homecoming）.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int a, b, c;
+
+    cin >> a >> b >> c;
+    if (c >= a && c < b) {
+        cout << "1\n";
+    } else {
+        cout << "0\n";
+    }
+
+    return 0;
+}
