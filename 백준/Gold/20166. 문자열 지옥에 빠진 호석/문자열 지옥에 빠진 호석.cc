4c549c12fb69ac6812f1d4c9d4040d2017087e03
ghyim <56685220+GihongYim@users.noreply.github.com>
2024-04-14 14:32:35 +0900

    [Gold IV] Title: 문자열 지옥에 빠진 호석, Time: 0 ms, Memory: 2296 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <string>
+using namespace std;
+int N, M, K;
+const int MAX = 11;
+string Map[MAX];
+map<string, int> m;
+set<string> s;
+
+int dx[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+void dfs(int x, int y, string str) {
+    int nx, ny;
+    if (s.find(str) == s.end())
+        return;
+    if (m.find(str) != m.end()) {
+        m[str]++;
+    }
+    for (int d = 0; d < 8; d++) {
+        nx = x + dx[d] + N;
+        ny = y + dy[d] + M;
+        nx %= N;
+        ny %= M;
+        dfs(nx, ny, str + Map[nx][ny]);
+    }
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
+    cin >> N >> M >> K;
+    for (int i = 0; i < N; i++) {
+        cin >> Map[i];
+    }
+    string tmp;
+    for (int i = 0; i < K; i++) {
+        cin >> tmp;
+        m.insert(make_pair(tmp, 0));
+        for (int j = 1; j <= tmp.length(); j++) {
+            s.insert(tmp.substr(0, j));
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            string start = "";
+            dfs(i, j, start + Map[i][j]);
+        }
+    }
+    for (auto u : m) {
+        cout << u.second << '\n';
+    }
+    return 0;
+}
