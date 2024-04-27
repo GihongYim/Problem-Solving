90b093f2b9cc96bc202e2b137c207c0546f63a52
ghyim <56685220+GihongYim@users.noreply.github.com>
2024-04-27 13:47:43 +0900

    [Gold V] Title: KSA 문자열, Time: 12 ms, Memory: 6508 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31433. KSA 문자열/KSA 문자열.cc
+++ 백준/Gold/31433. KSA 문자열/KSA 문자열.cc
@@ -11,42 +11,48 @@ int main(void)
     cin.tie(0); cout.tie(0);
     string x;
     string ksa = "KSA";
-    int curr = 0;
-    int startPos = 0;
+    string n[3];
     int cnt[3] = {0, 0, 0};
-    int removeCnt = 0;
+    int curr = 0;
     cin >> x;
     for (int p = 0; p < 3; p++) {
-        curr = 0;
+        curr = p;
         for (int i = 0; i < x.length(); i++) {
             if (x[i] == ksa[curr]) {
                 curr++;
                 curr %= 3;
-                cnt[p]++;
+                n[p] += x[i];
             }
         }
     }
-    // for (int i = 0; i < 3; i++)
-    //     cout << cnt[i] << '\n';
-    int maxP = 0;
-    int maxCnt = 0;
-    for (int i = 0; i < 3; i++) {
-        if (cnt[i] > maxCnt) {
-            maxP = i;
-            maxCnt = cnt[i];
+    for (int p = 0; p < 3; p++) {
+        cnt[p] = (x.length() - n[p].length());
+        if (n[p].length() == 0) {
+            n[p] = "K";
+            cnt[p]++;
         }
-    }
-    curr = maxP;
-    for (int i = 0; i < x.length(); i++) {
-        if (x[i] != ksa[curr]) {
-            x[i] = 'X';
-            removeCnt++;
-        } else {
-            curr++;
-            curr %= 3;
+        if (n[p][0] == 'A') {
+            n[p] = "S" + n[p];
+            cnt[p]++;
+            while (n[p].length() > x.length()) {
+                n[p].pop_back();
+                cnt[p]++;
+            }
         }
+        if (n[p][0] == 'S') {
+            n[p] = "K" + n[p];
+            cnt[p]++;
+            while (n[p].length() > x.length()) {
+                n[p].pop_back();
+                cnt[p]++;
+            }
+        }
+        cnt[p] += (x.length() - n[p].length());
     }
-    cout << removeCnt * 2 << '\n';
-
+    cout << *min_element(cnt, cnt + 3) << '\n';
     return 0;
 }
+
+// K(2) -> KSA(2) : 4
+// SAK(0) -> KSAK(1) -> KSA(1) : 2
+// AK(1) -> SAK(2) -> SA(1) -> KAS(1) : 6
