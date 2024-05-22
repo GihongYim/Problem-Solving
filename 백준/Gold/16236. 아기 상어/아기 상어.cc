9feef79b8c7b654a0a8610a24698c9390fd3afb3
ghyim <56685220+GihongYim@users.noreply.github.com>
2024-05-22 16:45:50 +0900

    [Gold III] Title: 아기 상어, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -1,90 +1,147 @@
 #include <stdio.h>
-#include <queue>
-using namespace std;
 
-struct SHARK {
-	int y, x, time;
-};
+const int MAX = 21;
+const int INF = 0x3f3f3f3f;
+typedef struct Point {
+    int x, y;
+} point;
 
-int n;
-int map[20][20];
+int N;
+int map[MAX][MAX];
+int dist[MAX][MAX];
 
-int shark_size, shark_eat;
-SHARK shark;
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
 
-const int dy[] = { -1,1,0,0 };
-const int dx[] = { 0,0,-1,1 };
+int sharkSize, sharkEat;
+int sharkX, sharkY;
 
-int main()
+point queue[500];
+int front;
+int end;
+
+int sec = 0;
+
+
+
+void queueInit() {
+    front = 0;
+    end = 0;
+}
+
+int queueEmpty() {
+    if (front == end) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void queuePush(point p) {
+    queue[end++] = p;
+}
+
+point queuePop() {
+    return queue[front++];
+}
+
+int isValid(int x, int y) {
+    if (x < 0 || x >= N || y < 0 || y >= N)
+        return 0;
+    else
+        return 1;
+}
+
+
+int bfs() {
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            dist[i][j] = INF;
+        }
+    }
+    queueInit();
+
+    point curr;
+    curr.x = sharkX;
+    curr.y = sharkY;
+    // printf("%d %d\n", sharkX, sharkY);
+    dist[curr.x][curr.y] = 0;
+    queuePush(curr);
+
+    while (!queueEmpty()) {
+        curr = queuePop();
+        // printf("%d %d\n", curr.x, curr.y);
+        for (int d = 0; d < 4; d++) {
+            point nxt;
+            nxt.x = curr.x + dx[d];
+            nxt.y = curr.y + dy[d];
+            if (!isValid(nxt.x, nxt.y)) continue;
+            if (dist[curr.x][curr.y] + 1 >= dist[nxt.x][nxt.y]) continue;
+            if (map[nxt.x][nxt.y] > sharkSize) continue;
+            dist[nxt.x][nxt.y] = dist[curr.x][curr.y] + 1;
+            queuePush(nxt);
+        }
+    }
+    int minDist = INF;
+    int minX = -1;
+    int minY = -1;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (dist[i][j] < INF && map[i][j] > 0 && map[i][j] < sharkSize) {
+                if (dist[i][j] < minDist) {
+                    minDist = dist[i][j];
+                    minX = i;
+                    minY = j;
+                }
+            }
+        }
+    }
+    // printf("bfs end\n");
+    // printf("minX: %d, minY : %d\n", minX, minY);
+    // printf("sharSize = %d\n", sharkSize);
+    // printf("map[3][0] = %d\n", map[3][0]);
+    // printf("dist[3][0] = %d\n", dist[3][0]);
+    // for (int i = 0; i < N; i++) {
+    //     for (int j = 0; j < N; j++) {
+    //         printf("%d ", map[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    if (minDist == INF) {
+        return 0;
+    } else {
+        sharkX = minX;
+        sharkY = minY;
+        map[sharkX][sharkY] = 0;
+        sec += minDist;
+        return 1;
+    }
+}
+
+int main(void)
 {
-	scanf("%d ", &n);
-	for (int y = 0; y < n; ++y) {
-		for (int x = 0; x < n; ++x) {
-			scanf("%d", &map[y][x]);
-			if (map[y][x] == 9) {
-				shark.y = y, shark.x = x, shark.time = 0;
-				shark_size = 2, shark_eat = 0;
-				map[y][x] = 0;
-			}
-		}
-	}
-	bool is_update = true;
-	while (is_update) {
-		is_update = false;
-
-		bool visited[20][20] = { false, };
-		queue<SHARK> q;
-		visited[shark.y][shark.x] = true;
-		q.push(shark);
-
-		SHARK candi;
-		candi.y = 20, candi.time = -1;
-
-		while (!q.empty()) {
-			SHARK cur = q.front(); q.pop(); 
-			
-			if (candi.time != -1 && candi.time < cur.time) {
-				break;
-			}
-
-			if (map[cur.y][cur.x] < shark_size && map[cur.y][cur.x] != 0) {
-				is_update = true;
-				if (candi.y > cur.y || (candi.y == cur.y && candi.x > cur.x)) {
-					candi = cur;
-				}
-			}
-
-			for (int dir = 0; dir < 4; ++dir) {
-				SHARK next;
-				next.y = cur.y + dy[dir];
-				next.x = cur.x + dx[dir];
-				next.time = cur.time + 1;
-
-				if (next.y < 0 || next.y >= n ||
-					next.x < 0 || next.x >= n) {
-					continue;
-				}
-
-				if (visited[next.y][next.x] == false && shark_size >= map[next.y][next.x]) {
-					visited[next.y][next.x] = true;
-					q.push(next);
-				}
-			}
-
-		}
-
-
-		if (is_update) {
-			shark = candi;
-			++shark_eat;
-			if (shark_eat == shark_size) {
-				shark_size++;
-				shark_eat = 0;
-			}
-			map[shark.y][shark.x] = 0;
-		}
-	}
-	printf("%d\n", shark.time);
-
-	return 0;
+    scanf("%d\n", &N);
+    sharkX = -1;
+    sharkY = -1;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &map[i][j]);
+            if (map[i][j] == 9) {
+                sharkX = i;
+                sharkY = j;
+                map[i][j] = 0;
+            }
+        }
+    }
+    sharkSize = 2;
+    sharkEat = 0;
+    while (bfs()) {
+        sharkEat += 1;
+        if (sharkSize == sharkEat) {
+            sharkSize += 1;
+            sharkEat = 0;
+        }
+    }
+    printf("%d\n", sec);
 }
