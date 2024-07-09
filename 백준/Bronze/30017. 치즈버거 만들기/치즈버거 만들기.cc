e9ef8568fe27fb3f9e44e1e34e5360428b095eb0
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2024-07-09 19:18:31 +0900

    [Bronze IV] Title: 치즈버거 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int A, B;
+
+	cin >> A >> B;
+
+	cout << 2 * min(A - 1, B) + 1 << '\n';
+	return 0;
+}
