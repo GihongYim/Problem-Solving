e1e2b727a1374feaf80b616ef3736199e020cd6e
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2024-07-15 20:07:32 +0900

    [Bronze III] Title: And Then There Was 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11520. And Then There Was 5/And Then There Was 5.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+
+	cin >> N;
+
+
+	for (int test_case = 1; test_case <= N; test_case++) {
+		int P, D;
+
+		cin >> P >> D;
+		cout << D << ' ' << 5 << '\n';
+	}
+
+
+
+	return 0;
+}
