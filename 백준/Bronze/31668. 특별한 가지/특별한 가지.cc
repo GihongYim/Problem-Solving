b04e16eac86c4186e9de5e7d525728de668a9eb6
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2024-07-22 19:43:58 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int N;
+
+	cin >> N;
+
+
+	int M;
+
+	cin >> M;
+
+	int K;
+
+	cin >> K;
+
+	cout << (M / N) * K << '\n';	
+
+	return 0;
+}
