1eb77e2d0161e5721649767f2fa13533f80418cb
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2025-01-29 15:08:27 +0900

    [Bronze I] Title: 2022년이 아름다웠던 이유, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27065. 2022년이 아름다웠던 이유/2022년이 아름다웠던 이유.cc
+++ 백준/Bronze/27065. 2022년이 아름다웠던 이유/2022년이 아름다웠던 이유.cc
@@ -1,65 +1,71 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int sumOfDivisors(int n) {
-    int sum = 1;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            sum += i;
-            if (i != n / i) {
-                sum += n / i;
-            }
-        }
-    }
-    return sum;
+int sumOfDivisors(int n)
+{
+	int sum = 1;
+
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			sum += i;
+			if (i != n / i) {
+				sum += n / i;
+			}
+		}
+	}
+	return sum;
 }
 
-bool isOverabundant(int n) {
-    return sumOfDivisors(n) > n;
+bool isAbundant(int n)
+{
+	return sumOfDivisors(n) > n;
 }
 
-bool isDeficient(int n) {
-    return sumOfDivisors(n) < n;
+bool isDeficiant(int n)
+{
+	return sumOfDivisors(n) < n;
 }
 
-bool isPerfect(int n) {
-    return sumOfDivisors(n) == n;
+bool isPerfect(int n)
+{
+	return sumOfDivisors(n) == n;
 }
 
-int main() {
-    int T;
-    cin >> T;
-    
-    while (T--) {
-        int n;
-        cin >> n;
-        
-        if (!isOverabundant(n)) {
-            cout << "BOJ 2022" << endl;
-            continue;
-        }
-        
-        bool valid = true;
-        for (int i = 1; i * i <= n; i++) {
-            if (n % i == 0) {
-                if (i != n && !isDeficient(i) && !isPerfect(i)) {
-                    valid = false;
-                    break;
-                }
-                if (i != 1 && i != n / i && !isDeficient(n / i) && !isPerfect(n / i)) {
-                    valid = false;
-                    break;
-                }
-            }
-        }
-        
-        if (valid) {
-            cout << "Good Bye" << endl;
-        } else {
-            cout << "BOJ 2022" << endl;
-        }
-    }
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    return 0;
+	int T;
+	int n;
+
+	cin >> T;
+
+	for (int t = 1; t <= T; t++) {
+		cin >> n;
+		if (!isAbundant(n)) {
+			cout << "BOJ 2022\n";
+			continue;
+		} 
+		bool valid = true;
+		for (int i = 1; i * i <= n; i++) {
+			if (n % i == 0) {
+				if (i != n && !isDeficiant(i) && !isPerfect(i)) {
+					valid = false;
+					break;
+				}
+				if (i != 1 && i != n / i && !isDeficiant(n / i) && !isPerfect(n / i)) {
+					valid = false;
+					break;
+				}
+			}
+		}
+		if (valid) {
+			cout << "Good Bye\n";
+		} else {
+			cout << "BOJ 2022\n";
+		}
+	}
+
+	return 0;
 }
