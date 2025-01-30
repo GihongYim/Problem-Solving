0047927ff817c389c4ab6051cc6fe0625730b73b
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2025-01-30 09:04:05 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int T, V;
+	cin >> T >> V;
+
+	cout << T * V << '\n';
+
+	return 0;
+}
