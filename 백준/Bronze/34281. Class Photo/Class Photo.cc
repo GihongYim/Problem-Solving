d9c9dbbec421fd7e1f33ffad8190bb8c56294506
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2025-09-12 22:57:30 +0900

    [Bronze V] Title: Class Photo, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34281. Class Photo/Class Photo.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int w, l;
+
+    cin >> w >> l;
+
+    cout << w * l << '\n';
+
+    return 0;
+}
