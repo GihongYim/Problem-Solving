24b9c23dbcdb05102c44e0027ca6589572039f75
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2025-12-06 10:53:47 +0900

    [Bronze III] Title: Ответный матч, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/29667. Ответный матч/Ответный матч.cc
+++ 백준/Bronze/29667. Ответный матч/Ответный матч.cc
@@ -8,35 +8,20 @@ int main() {
     string s1, s2;
     cin >> s1 >> s2;
 
-    int aHome = s1[0] - '0';
-    int bAway = s1[2] - '0';
+    int aH = s1[0] - '0';
+    int bA = s1[2] - '0';
+    int bH = s2[0] - '0';
+    int aA = s2[2] - '0';
 
-    int bHome_now = s2[0] - '0';
-    int aAway_now = s2[2] - '0';
-
-    int A_total_now = aHome + aAway_now;
-    int B_total_now = bAway + bHome_now;
-
-    int A_away_now = aAway_now;  
-    int B_away_now = bAway;      
-
- 
-    for (int addA = 0; addA <= 20; addA++) {
-        for (int addB = 0; addB <= 20; addB++) {
-
-            int A_total = A_total_now + addA;
-            int B_total = B_total_now + addB;
-
-            int A_away = A_away_now + addA; 
-            int B_away = B_away_now;        
-
-            if (A_total == B_total && A_away == B_away) {
-                cout << "YES\n";
-                return 0;
-            }
-        }
+    int x = bA - aA;
+    if (x < 0) {
+        cout << "NO\n";
+        return 0;
     }
 
-    cout << "NO\n";
-    return 0;
+    int D = (bA + bH) - (aH + aA);
+    int y = x - D;
+
+    if (y < 0) cout << "NO\n";
+    else cout << "YES\n";
 }
