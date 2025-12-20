99a29381ad82bdee1d16e278d23f6cd7b95578b9
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2025-12-20 16:21:34 +0900

    [Bronze IV] Title: Blaster's Dash, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34380. Blaster's Dash/Blaster's Dash.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int m, v;
+
+    int n;
+    int k;
+
+    cin >> m >> v;
+
+    cin >> n;
+    cin >> k;
+
+    cout << 2 * (20 + n) << '\n';
+    return 0;
+}
