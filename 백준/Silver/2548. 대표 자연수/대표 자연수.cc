02c14f75d5d23644b259aa3da3f1a4e53ce7af88
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2026-01-31 10:45:29 +0900

    [Silver III] Title: 대표 자연수, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Silver/2548. 대표 자연수/대표 자연수.cc
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.cc
@@ -4,39 +4,17 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
     int N;
-    
     cin >> N;
-    vector<int> numbers(N);
+
+    vector<int> a(N);
     for (int i = 0; i < N; i++) {
-        cin >> numbers[i];
-    }
-    
-    sort(numbers.begin(), numbers.end());
-    vector<int> acc(N, 0);
-    acc[0] = numbers[0];    
-    for (int i = 1; i < N; i++) {
-        acc[i] = acc[i - 1] + numbers[i];
+        cin >> a[i];
     }
 
-    int minIndex = 0;
-    int minDiff = (acc[N - 1] - acc[0]) - numbers[0] * (N - 1);
-    for (int i = 1; i < N - 1; i++) {
-        int lowerSum = acc[i - 1];
-        int upperSum = acc[N - 1] - acc[i];
-        int diff = abs(i * numbers[i] - lowerSum + upperSum - (N - i - 1) * numbers[i]);
-        if (diff < minDiff) {
-            minDiff = diff;
-            minIndex = i;
-        }
-    }
+    sort(a.begin(), a.end());
 
-    if ((N - 1) * numbers[N - 1] - acc[N - 2] < minDiff) {
-        minIndex = N - 1;
-    }
-
-    cout << numbers[minIndex] << '\n';
+    cout << a[(N - 1) / 2] << '\n';
     return 0;
 }
