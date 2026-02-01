bfbf737cb83dbe199d87ab8bf7bcfded42bdcf8c
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2026-02-01 09:58:16 +0900

    [Silver III] Title: 줄줄이 박수, Time: 252 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Silver/29718. 줄줄이 박수/줄줄이 박수.cc
+++ 백준/Silver/29718. 줄줄이 박수/줄줄이 박수.cc
@@ -4,35 +4,35 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
     int N, M;
-    int A;
     cin >> N >> M;
-    vector<vector<int>> q(N + 1, vector<int>(M + 1, 0));
+
+    vector<int> colSum(M + 1, 0);
 
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= M; j++) {
-            cin >> q[i][j];
+            int x;
+            cin >> x;
+            colSum[j] += x;
         }
     }
 
+    int A;
     cin >> A;
-    vector<int> colSum(M + 1, 0);
+
+    vector<long long> prefix(M + 1, 0);
     for (int j = 1; j <= M; j++) {
-        for (int i = 1; i <= N; i++) {
-            colSum[j] += q[i][j];
-        }
+        prefix[j] = prefix[j - 1] + colSum[j];
     }
 
-    int maxSum = 0;
-    for (int i = 1; i <= M - A + 1; i++) {
-        int sum = 0;
-        for (int j = i; j < i + A; j++) {
-            sum += colSum[j];
-        }
-        maxSum = max(maxSum, sum);
+    long long ans = 0;
+
+    for (int l = 1; l + A - 1 <= M; l++) {
+        long long curr = prefix[l + A - 1] - prefix[l - 1];
+        ans = max(ans, curr);
     }
-    cout << maxSum << "\n";
+
+    cout << ans << '\n';
     return 0;
 }
