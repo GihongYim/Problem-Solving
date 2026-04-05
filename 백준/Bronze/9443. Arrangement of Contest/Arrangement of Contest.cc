eb0d82ce845f83708a1137c2efcf47fa0d2f2ebe
VOIDRAY <56685220+GihongYim@users.noreply.github.com>
2026-04-05 09:13:55 +0900

    [Bronze III] Title: Arrangement of Contest, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/9443. Arrangement of Contest/Arrangement of Contest.cc
+++ 백준/Bronze/9443. Arrangement of Contest/Arrangement of Contest.cc
@@ -4,30 +4,23 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
     int n;
-
     cin >> n;
-    vector<string> s(n);
+
+    vector<bool> exist(26, false);
 
     for (int i = 0; i < n; i++) {
-        cin >> s[i];
+        string s;
+        cin >> s;
+        exist[s[0] - 'A'] = true;
     }
 
-    sort(s.begin(), s.end());
-
-    char ch = 'A';
     int cnt = 0;
-    for (int i = 0; i < n; i++) {
-        if (s[i][0] == ch) {
-            cnt++;
-            ch++;
-        } else if (s[i][0] > ch) {
-            break;
-        }
+    for (int i = 0; i < 26; i++) {
+        if (exist[i]) cnt++;
+        else break;
     }
-    cout << cnt << endl;
 
-    return 0;
+    cout << cnt << '\n';
 }
